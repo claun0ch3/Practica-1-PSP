55a213aa187e137065535c55d4c26050c09d9216
clau <claudiarock10@gmail.com>
2025-09-19 12:01:29 +0200

    Calculadora Version Actualizada

--- LanzarCalculadora/src/Main.java
+++ LanzarCalculadora/src/Main.java
@@ -2,21 +2,38 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        Runtime rt = Runtime.getRuntime();
         try {
-            String[] comando = {"Calculadora.jar"};
-            Process p = rt.exec(comando);
+            ProcessBuilder pb = new ProcessBuilder("java", "-jar", "..\\Calculadora_jar\\Calculadora.jar");
+            Process proceso = pb.start();
 
-            InputStream is = p.getInputStream();
+            InputStream is = proceso.getInputStream();
             InputStreamReader isr = new InputStreamReader(is);
             BufferedReader br = new BufferedReader(isr);
             String linea = "";
+            linea = br.readLine();
+            System.out.println(linea);
+            linea = br.readLine();
+            System.out.println(linea);
 
-            while ((linea = br.readLine()) != null) { // Si la linea no es null, la muestra
-                System.out.println(linea);
-            }
+            OutputStream os = proceso.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(os);
+            BufferedWriter bw = new BufferedWriter(osw);
+            bw.write(3 + "\n");
+            bw.flush();
+            linea = br.readLine();
+            System.out.println(linea);
+            bw.write(3 + "\n");
+            bw.flush();
+            linea = br.readLine();
+            System.out.println(linea);
+            bw.write('+' + "\n");
+            bw.flush();
+            linea = br.readLine();
+            System.out.println(linea);
+
+
         } catch (IOException e) {
-            System.out.println("Ha ocurrido un error de E/S");
+            System.err.println("Error al lanzar la calculadora: " + e.getMessage());
         }
     }
 }
